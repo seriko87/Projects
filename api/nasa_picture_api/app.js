da972ed0f9280787cd57ce100cdbd094d8c9af1f
seriko87 <58387052+seriko87@users.noreply.github.com>
2020-12-29 19:29:44 -0500

    new

---
+++ api/nasa_picture_api/app.js
@@ -0,0 +1,45 @@
+/*
+function updateApi (){
+    $.ajax({
+         url: `https://api.nasa.gov/planetary/apod?`,
+         data:{date:'2020-10-10',api_key: 'NaQ9geoTkHNXybutx7eD9h3zjilArlXgOd3fydf8'},
+         type: 'GET',
+         datatype: 'json',
+         success: function(data){
+                updateInterface(data);
+                console.log(data)
+         }
+     });
+ };
+*/
+
+
+$('#start').on("click", ()=> {
+    updateApi();
+    $('#start').hide();
+    $('#main').show();
+} )
+
+function updateInterface(data){
+    let url = data.url;
+    $('img').attr('src', url);
+    $('#title').text(data.title);
+    $('#expln').text(data.explanation);
+}
+
+const getData = async ( url = 'https://api.nasa.gov/planetary/apod?api_key=NaQ9geoTkHNXybutx7eD9h3zjilArlXgOd3fydf8', data = {})=>{
+    let url = new URL(url)
+    URLSearchParams
+      const response = await fetch(url);
+      
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+}
+
+getData();
